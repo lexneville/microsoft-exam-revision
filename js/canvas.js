8c3ec400c7bc25e7c8baefa3f250e30eb9f88888
Jody Richardson <jody@beeta.co>
2020-01-29 14:51:02 +0000

    added git ignore

--- js/canvas.js
+++ js/canvas.js
@@ -17,7 +17,7 @@ window.onload = function () {
 
     ctx.beginPath();
     // ctx.moveTo(10, 100);
-    ctx.arc(150, 100, 75, 0, 2 * Math.PI, false);
+    ctx.arc(150, 100, 100, 0, 2 * Math.PI, false);
     ctx.lineWidth = 25;
     ctx.strokeStyle = "#0f0";
     ctx.lineCap = "round";
@@ -38,5 +38,33 @@ window.onload = function () {
     ctx.strokeStyle = "#0f0";
     ctx.lineCap = "round";
     ctx.stroke();
+
+    // Quadratic arc
+    ctx.beginPath();
+    ctx.moveTo(10, 380);
+    ctx.quadraticCurveTo(300, -250, 580, 380);
+    ctx.lineWidth = 25;
+    ctx.strokeStyle = '#f00';
+    ctx.stroke();
+
+    // Bezier Cruve
+    ctx.beginPath();
+    ctx.moveTo(125, 20);
+    ctx.bezierCurveTo(0, 200, 300, 300, 50, 400);
+    ctx.lineWidth = 5;
+    ctx.strokeStyle = '#d50';
+    ctx.stroke();
+
+    ctx.beginPath();
+    ctx.arc(300, 200, 75, 1.75 * Math.PI, 1.25 * Math.PI, false);
+    ctx.lineTo(150, 125);
+    ctx.quadraticCurveTo(300, 0, 450, 125);
+    ctx.lineTo(353, 144);
+    ctx.strokeStyle = "blue";
+    ctx.lineCap = "round";
+    ctx.lineWidth = 10;
+    ctx.stroke();
+
+
 
 }
